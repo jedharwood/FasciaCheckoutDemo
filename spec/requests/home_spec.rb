2bc647e0138c4c7d407e269eb4919733df36db1d
Jed Harwood <scaryteeth@hotmail.com>
2023-01-05 12:28:51 +0900

    home controller plus tests

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -3,11 +3,38 @@
 require 'rails_helper'
 
 RSpec.describe 'Homes', type: :request do
+  before(:example, populate_materials: true) do
+    @materials = []
+    3.times do |i|
+      @materials << Material.create(name: "Material #{i}", description: "Material #{i} description", thickness: i, width: i)
+    end
+    @materials.sort_by { :name }
+  end
+
   describe 'GET /index' do
-    it 'succeeds' do
-      get root_path
-      expect(response).to be_successful
-      expect(response).to render_template(:index)
+    context 'when db contains materials', populate_materials: true do
+      it 'succeeds' do
+        get root_path
+        expect(response).to be_successful
+        expect(response).to render_template(:index)
+      end
+
+      it 'renders index template' do
+        get root_path
+        expect(response).to render_template(:index)
+      end
+
+      it 'assigns @materials' do
+        get root_path
+        expect(assigns(:materials)).to eq(@materials)
+      end
+    end
+
+    context 'when db contains no materials' do
+      it 'assigns @materials as an empty array' do
+        get root_path
+        expect(assigns(:materials)).to eq([])
+      end
     end
   end
 end
