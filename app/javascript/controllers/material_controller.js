58af670881ea214083d11f405ad9b00766721992
Jed Harwood <scaryteeth@hotmail.com>
2023-01-10 11:44:44 +0900

    created modal and material controller to render modal on material info partial

---
+++ app/javascript/controllers/material_controller.js
@@ -0,0 +1,26 @@
+import { Controller } from '@hotwired/stimulus';
+import { enter, leave } from 'el-transition';
+
+export default class extends Controller {
+    connect() {
+      document.getElementById('modal-wrapper').addEventListener('click', (event) => {
+        this.closeModal(event);
+      });
+    }
+  
+    closeModal(event) {
+      const modalPanelClicked = document.getElementById('modal-panel').contains(event.target);
+  
+      if (!modalPanelClicked) {
+        leave(document.getElementById('modal-wrapper'));
+        leave(document.getElementById('modal-backdrop'));
+        leave(document.getElementById('modal-panel'));
+      }
+    }
+  
+    showModal() {
+      enter(document.getElementById('modal-wrapper'));
+      enter(document.getElementById('modal-backdrop'));
+      enter(document.getElementById('modal-panel'));
+    }
+}
