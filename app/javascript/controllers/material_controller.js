d55aa2590c44f5356a9d2622cde962d5d3ff5195
Jed Harwood <scaryteeth@hotmail.com>
2023-01-10 12:06:57 +0900

    modal close button

--- app/javascript/controllers/material_controller.js
+++ app/javascript/controllers/material_controller.js
@@ -2,25 +2,35 @@ import { Controller } from '@hotwired/stimulus';
 import { enter, leave } from 'el-transition';
 
 export default class extends Controller {
-    connect() {
-      document.getElementById('modal-wrapper').addEventListener('click', (event) => {
-        this.closeModal(event);
-      });
+  static targets = ['closeButton'];
+
+  connect() {
+    document.getElementById('modal-wrapper').addEventListener('click', (event) => {
+      this.closeModal(event);
+    });
+
+    this.closeButtonTarget.addEventListener('click', () => {
+      this.leaveModal();
+    });
+  }
+
+  closeModal(event) {
+    const modalPanelClicked = document.getElementById('modal-panel').contains(event.target);
+
+    if (!modalPanelClicked) {
+      this.leaveModal();
     }
-  
-    closeModal(event) {
-      const modalPanelClicked = document.getElementById('modal-panel').contains(event.target);
-  
-      if (!modalPanelClicked) {
-        leave(document.getElementById('modal-wrapper'));
-        leave(document.getElementById('modal-backdrop'));
-        leave(document.getElementById('modal-panel'));
-      }
-    }
-  
-    showModal() {
-      enter(document.getElementById('modal-wrapper'));
-      enter(document.getElementById('modal-backdrop'));
-      enter(document.getElementById('modal-panel'));
-    }
+  }
+
+  showModal() {
+    enter(document.getElementById('modal-wrapper'));
+    enter(document.getElementById('modal-backdrop'));
+    enter(document.getElementById('modal-panel'));
+  }
+
+  leaveModal() {
+    leave(document.getElementById('modal-wrapper'));
+    leave(document.getElementById('modal-backdrop'));
+    leave(document.getElementById('modal-panel'));
+  }
 }
