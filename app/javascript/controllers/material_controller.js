8dc0c687b11a24989d420766b06523693cd2d8b3
Jed Harwood <scaryteeth@hotmail.com>
2023-01-11 11:40:16 +0900

    updated js controller ad material view to handle basket submission

--- app/javascript/controllers/material_controller.js
+++ app/javascript/controllers/material_controller.js
@@ -1,7 +1,10 @@
 import { Controller } from '@hotwired/stimulus';
+import Swal from 'sweetalert2';
 
 export default class extends Controller {
   static targets = ['lengthInput', 'numberOfMetres', 'materialCost', 'materialTotal'];
+  HEADERS = { ACCEPT: 'application/json' };
+  BASKET_PATH = '/api/basket';
 
   showDescription(e) {
     e.preventDefault();
@@ -9,14 +12,55 @@ export default class extends Controller {
   }
 
   handleInputChange() {
-    this.numberOfMetresTarget.textContent = this.lengthInputTarget.value;
+    this.numberOfMetresTarget.textContent = this.getLength();
     this.materialCostTarget.textContent = this.calculateCost();
     this.materialTotalTarget.textContent = this.calculateCost();
+  }
+
+  getLength() {
+    return this.lengthInputTarget.value ? this.lengthInputTarget.value : 0;
   }
 
   calculateCost() {
     const length = this.lengthInputTarget.value;
     const pricePerMetreInCents = parseFloat(this.element.dataset.pricePerMetre.replace(',', ''));
     return length ? pricePerMetreInCents * parseInt(length) : 0;
+  }
+
+  addToBasket() {
+    if (this.lengthInputTarget.value < 1) {
+      Swal.fire({
+        text: 'Please tell us how many metres you require',
+        icon: 'warning',
+        confirmButtonColor: '#f59e0b',
+      }); 
+      return;
+    }
+
+    let formData = new FormData();
+    formData.append('material_id', this.element.dataset.materialId);
+    formData.append('length', this.lengthInputTarget.value);
+
+    fetch(this.BASKET_PATH, {
+      method: 'POST',
+      headers: this.headers,
+      body: formData
+    })
+    .then(response => {
+      if (response.ok) {
+        // emit event to checkout controller
+        Swal.fire({
+          text: 'Material successfully added to cart',
+          icon: 'success',
+          confirmButtonColor: '#14b8a6',
+        });
+      } else {
+        Swal.fire({
+          text: 'Something went wrong, please try again',
+          icon: 'error',
+          confirmButtonColor: '#f43f5e',
+        });
+      }
+    });
   }
 }
--- config/importmap.rb
+++ config/importmap.rb
@@ -8,3 +8,4 @@ pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
 pin_all_from 'app/javascript/controllers', under: 'controllers'
 pin 'el-transition', to: 'https://ga.jspm.io/npm:el-transition@0.0.7/index.js'
+pin "sweetalert2", to: "https://ga.jspm.io/npm:sweetalert2@11.6.16/dist/sweetalert2.all.js"
